cdbd5ca0e2962bec673fb2667d99f9ff6e7f4bd0
ChaseSGithub <114618426+ChaseSGithub@users.noreply.github.com>
2022-10-13 17:07:55 -0700

    Things for Lab 3

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,9 +4,15 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int[] temp = new int[arr.length];
+      for(int n = 0; n<arr.length; n++){
+        temp[n] = arr[n];
+      }
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      arr[i] = temp[temp.length-i-1];
+      
     }
+
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
@@ -14,14 +20,14 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
   // lowest number when calculating. Returns 0 if there are no elements or just
-  // 1 element in the array
+  // 1 element in the array [0,1,2,3,4] [4,1,2,3,4] [4,3,2,3,4]
   static double averageWithoutLowest(double[] arr) {
     if(arr.length < 2) { return 0.0; }
     double lowest = arr[0];
